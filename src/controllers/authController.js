a34752e521ba8a45a83958f0fd32ebd29d0c17f8
rafaelmoreno21 <rafaelmoreno210900@gmail.com>
2020-09-24 18:25:52 -0400

    first commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,60 @@
+const { Router } = require('express');
+const router = Router();
+
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const verifyToken = require('./verifyToken');
+const User = require('../models/User');
+
+
+router.post('/signup', async (req, res, next) => {
+    const { username, email, password } = req.body;
+    // Creating a new User
+    const user = new User({
+        username,
+        email,
+        password
+    });
+    user.password = await user.encryptPassword(password);
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 60 * 60 * 24
+    })
+
+    res.json({ auth: true, token });
+
+});
+
+router.get('/me', verifyToken, async (req, res, next) => {
+
+    const user = await User.findById(req.userId, { password: 0 });
+    if (!user) {
+        return res.status(404).send('No user found');
+    }
+    res.json(user);
+});
+
+router.post('/signin', async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(404).send('The email doesnt exists');
+    }
+    const passwordValid = await user.comparePassword(password);
+    if (!passwordValid) {
+        return res.status(401).json({ auth: false, token: null })
+    }
+
+    const token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 60 * 60 * 24
+    })
+
+    res.json({ auth: true, token })
+});
+
+
+
+
+module.exports = router;
